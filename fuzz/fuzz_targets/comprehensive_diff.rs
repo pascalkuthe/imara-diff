59c0f85d6f8e2fc4add3988f07ab44d17c996877
Sebastian Thiel <sebastian.thiel@icloud.com>
2025-12-30 10:13:13 +0100

    Also fuzz word diffs

--- fuzz/fuzz_targets/comprehensive_diff.rs
+++ fuzz/fuzz_targets/comprehensive_diff.rs
@@ -8,7 +8,9 @@ use libfuzzer_sys::arbitrary;
 #[derive(arbitrary::Arbitrary, Debug)]
 struct Input<'a> {
     before: &'a [u8],
+    before_str: &'a str,
     after: &'a [u8],
+    after_str: &'a str,
 }
 
 /// Tests all three diff algorithms (Myers, Histogram, MyersMinimal) with:
@@ -17,7 +19,14 @@ struct Input<'a> {
 /// - Unified diff printing
 /// - Basic queries (count_additions, count_removals, is_added, is_removed)
 /// - Hunks iteration
-fn do_fuzz(Input { before, after }: Input<'_>) {
+fn do_fuzz(
+    Input {
+        before,
+        before_str,
+        after,
+        after_str,
+    }: Input<'_>,
+) {
     // Create interned input
     let input = InternedInput::new(before, after);
 
@@ -46,6 +55,15 @@ fn do_fuzz(Input { before, after }: Input<'_>) {
 
         // Test postprocessing with line heuristic
         diff.postprocess_lines(&input);
+    }
+
+    let input = InternedInput::new(before_str, after_str);
+    let mut word_input = InternedInput::default();
+    let mut word_diff = Diff::default();
+
+    let diff = Diff::compute(Algorithm::Myers, &input);
+    for hunk in diff.hunks() {
+        hunk.latin_word_diff(&input, &mut word_input, &mut word_diff);
     }
 }
 
